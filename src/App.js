8cb729528aee92591af5eb21425c1fc6d98c6d0a
saqib nazir <saqib_9090@yahoo.com>
2022-07-13 01:23:47 +0100

    all menu  button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import items from './data';
 import Menu from './Menu';
 
 function App() {
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
   return (
@@ -13,7 +21,7 @@ function App() {
           <h2>Our Menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Categories = () => {
-  return <div>Categories</div>;
+const Categories = ({ filterItems }) => {
+  return (
+    <div className="btn-container">
+      <button className="filter-btn" onClick={() => filterItems('all')}>
+        All
+      </button>
+      <button className="filter-btn" onClick={() => filterItems('breakfast')}>
+        breakfast
+      </button>
+    </div>
+  );
 };
 
 export default Categories;
